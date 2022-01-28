78df2d859e94c3b70b582c44e8553319b8208c3c
Khiem Pham <khiempham95@hotmail.com>
2022-01-28 00:26:53 -0800

    Add Unit test base + Add Moq NuGet Package

---
+++ CoxAutomotiveCodingExercise.API.Tests/Services/DataSetServiceTests/CreateAnswerTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using CoxAutomotiveCodingExercise.API.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace CoxAutomotiveCodingExercise.API.Tests.Services.DataSetServiceTests
+{
+    public class CreateAnswerTests
+    {
+        private DataSetService dataSetService;
+
+        [SetUp]
+        public void Setup()
+        {
+            var coxAutoClientServiceMock = new Mock<ICoxAutoClientService>();
+            var mapperMock = new Mock<IMapper>();
+            dataSetService = new DataSetService(coxAutoClientServiceMock.Object, mapperMock.Object);
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
